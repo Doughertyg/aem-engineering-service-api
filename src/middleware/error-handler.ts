94ab01d5c2d3e3173372fe24fd0b2a6a913717db
doughertyg <doughertyg37@gmail.com>
2025-01-24 02:16:55 -0800

    add error message

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { APIError } from '../utils/custom-error';
+import { ERROR_MESSAGES } from '../utils/error-messages';
 
 /**
  * Middleware for handling errors in the server
@@ -14,5 +15,5 @@ export const errorHandlerMiddleware = (
 
   res
     .status(err.statusCode || 500)
-    .send(err.message || 'Internal Server Error');
+    .send(err.message || ERROR_MESSAGES.INTERNAL_ERROR);
 };
